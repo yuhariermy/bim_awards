9789c71e0cb9f451320f68adf93a7622bcd1dd83
yuhariermy <yuhariermy@gmail.com>
2022-02-08 09:51:00 +0700

    add user routes

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/admin', function () {
     return view('welcome');
+});
+Route::get('/', function () {
+    return view('user');
 });
 
 Auth::routes();
